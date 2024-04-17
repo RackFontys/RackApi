ef6b7d12bcfe04cb0470789e8a62be6b4febe261
JornKersten <jorn-2002@live.nl>
2024-04-17 13:51:06 +0200

    Ci test

--- RackApi.Chat/Controllers/MessageController.cs
+++ RackApi.Chat/Controllers/MessageController.cs
@@ -44,7 +44,6 @@ public class MessageController : ControllerBase
     [HttpDelete]
     public async Task<ActionResult<String>> DeleteMessage(int userId)
     {
-        Console.WriteLine("test");
         var messagesToDelete = await _context.Messages.Where(x => x.ToUserId == userId || x.UserId == userId).ToListAsync();
         
         bool isNotEmpty = messagesToDelete.Any();
