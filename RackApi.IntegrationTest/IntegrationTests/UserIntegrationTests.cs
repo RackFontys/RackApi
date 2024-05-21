c105d785198e3562f6b999d6e25607daa534a750
JornKersten <jorn-2002@live.nl>
2024-05-21 15:13:34 +0200

    Test

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -7,8 +7,6 @@ public class UserIntegrationTests
 {
     private HttpClient _client;
     private HttpClient _client2;
-    private HttpClient _client3;
-    private HttpClient _client4;
 
     [SetUp]
     public void Setup()
@@ -69,66 +67,6 @@ public class UserIntegrationTests
         // Assert
         response.EnsureSuccessStatusCode();
     }
-    
-    [SetUp]
-    public void Setup2()
-    {
-        // Initialize HttpClient
-        _client3 = new HttpClient();
-        _client3.BaseAddress = new Uri("http://rackapi_api_1:80");
-    }
-
-    [Test]
-    public async Task Test_User_Creation2()
-    {
-        // Arrange
-        var user = new 
-        { 
-            Id = 0,
-            Name = "Test User", 
-            Email = "test@example.com", 
-            CreatedAt = DateTime.UtcNow,
-            CompanyId = 1,
-            Password = "testpassword"
-        };
-        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
-
-        // Act
-        var response = await _client3.PostAsync("/User", content);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-    }
-    
-    [SetUp]
-    public void SetupBasicUser2()
-    {
-        // Initialize HttpClient
-        _client4 = new HttpClient();
-        _client4.BaseAddress = new Uri("http://rackapi_user_1:5114");
-    }
-
-    [Test]
-    public async Task Test_User_Creation_User_Level2()
-    {
-        // Arrange
-        var user = new 
-        { 
-            Id = 0,
-            Name = "Test User", 
-            Email = "test@example.com", 
-            CreatedAt = DateTime.UtcNow,
-            CompanyId = 1,
-            Password = "testpassword"
-        };
-        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
-
-        // Act
-        var response = await _client4.PostAsync(_client4.BaseAddress, content);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-    }
 
     [TearDown]
     public void TearDown()
@@ -136,7 +74,5 @@ public class UserIntegrationTests
         // Initialize HttpClient
         _client.Dispose();
         _client2.Dispose();
-        _client3.Dispose();
-        _client4.Dispose();
     }
 }
