d3135388d1570dab7b20a2f3e8291ee8f38dde26
JornKersten <jorn-2002@live.nl>
2024-05-21 13:48:41 +0200

    Update testing file

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -12,7 +12,7 @@ public class UserIntegrationTests
     {
         // Initialize HttpClient
         _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://api:5283");
+        _client.BaseAddress = new Uri("http://api:80");
     }
 
     [Test]
@@ -61,7 +61,67 @@ public class UserIntegrationTests
         var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
 
         // Act
+        var response = await _client.PostAsync(_client.BaseAddress, content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+    
+    [SetUp]
+    public void Setup2()
+    {
+        // Initialize HttpClient
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri("http://rackapi_api_1:80");
+    }
+
+    [Test]
+    public async Task Test_User_Creation2()
+    {
+        // Arrange
+        var user = new 
+        { 
+            Id = 0,
+            Name = "Test User", 
+            Email = "test@example.com", 
+            CreatedAt = DateTime.UtcNow,
+            CompanyId = 1,
+            Password = "testpassword"
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+        // Act
         var response = await _client.PostAsync("/User", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+    
+    [SetUp]
+    public void SetupBasicUser2()
+    {
+        // Initialize HttpClient
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri("http://rackapi_user_1:5114");
+    }
+
+    [Test]
+    public async Task Test_User_Creation_User_Level2()
+    {
+        // Arrange
+        var user = new 
+        { 
+            Id = 0,
+            Name = "Test User", 
+            Email = "test@example.com", 
+            CreatedAt = DateTime.UtcNow,
+            CompanyId = 1,
+            Password = "testpassword"
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync(_client.BaseAddress, content);
 
         // Assert
         response.EnsureSuccessStatusCode();
