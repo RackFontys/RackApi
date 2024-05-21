1f9d320284a14ffebbf8f5adb6154f809d071bad
JornKersten <jorn-2002@live.nl>
2024-05-21 12:16:20 +0200

    Update adress

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -12,7 +12,7 @@ public class UserIntegrationTests
     {
         // Initialize HttpClient
         _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://localhost:5283");
+        _client.BaseAddress = new Uri("http://api:5283");
     }
 
     [Test]
