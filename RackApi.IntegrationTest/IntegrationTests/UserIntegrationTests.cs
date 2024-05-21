3c9e0af7f1882f2c2a2fec2f522b050aa8c5c1c4
JornKersten <jorn-2002@live.nl>
2024-05-21 13:55:31 +0200

    Change to testfile

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -6,6 +6,9 @@ namespace RackApi.IntegrationTest;
 public class UserIntegrationTests
 {
     private HttpClient _client;
+    private HttpClient _client2;
+    private HttpClient _client3;
+    private HttpClient _client4;
 
     [SetUp]
     public void Setup()
@@ -41,8 +44,8 @@ public class UserIntegrationTests
     public void SetupBasicUser()
     {
         // Initialize HttpClient
-        _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://user:5114");
+        _client2 = new HttpClient();
+        _client2.BaseAddress = new Uri("http://user:5114");
     }
 
     [Test]
@@ -61,7 +64,7 @@ public class UserIntegrationTests
         var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
 
         // Act
-        var response = await _client.PostAsync(_client.BaseAddress, content);
+        var response = await _client.PostAsync(_client2.BaseAddress, content);
 
         // Assert
         response.EnsureSuccessStatusCode();
@@ -71,8 +74,8 @@ public class UserIntegrationTests
     public void Setup2()
     {
         // Initialize HttpClient
-        _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://rackapi_api_1:80");
+        _client3 = new HttpClient();
+        _client3.BaseAddress = new Uri("http://rackapi_api_1:80");
     }
 
     [Test]
@@ -101,8 +104,8 @@ public class UserIntegrationTests
     public void SetupBasicUser2()
     {
         // Initialize HttpClient
-        _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://rackapi_user_1:5114");
+        _client4 = new HttpClient();
+        _client4.BaseAddress = new Uri("http://rackapi_user_1:5114");
     }
 
     [Test]
@@ -132,5 +135,8 @@ public class UserIntegrationTests
     {
         // Initialize HttpClient
         _client.Dispose();
+        _client2.Dispose();
+        _client3.Dispose();
+        _client4.Dispose();
     }
 }
