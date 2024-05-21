ea40bd9d3a9f5bcbb35d2d1d9d960b5cbd014087
JornKersten <jorn-2002@live.nl>
2024-05-21 13:44:04 +0200

    Test different host

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -36,6 +36,36 @@ public class UserIntegrationTests
         // Assert
         response.EnsureSuccessStatusCode();
     }
+    
+    [SetUp]
+    public void SetupBasicUser()
+    {
+        // Initialize HttpClient
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri("http://user:5114");
+    }
+
+    [Test]
+    public async Task Test_User_Creation_User_Level()
+    {
+        // Arrange
+        var user = new 
+        { 
+            Id = 0,
+            Name = "Test User", 
+            Email = "test@example.com", 
+            CreatedAt = DateTime.UtcNow,
+            CompanyId = 1,
+            Password = "testpassword"
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/User", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
 
     [TearDown]
     public void TearDown()
