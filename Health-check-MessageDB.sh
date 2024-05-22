815be0563ddbb9b42e1b3bd38b85b3462abb8b69
JornKersten <jorn-2002@live.nl>
2024-05-22 13:16:48 +0200

    Implemented Health check

---
+++ Health-check-MessageDB.sh
@@ -0,0 +1,6 @@
+﻿until pg_isready -U postgres; do
+    sleep 5
+    echo "Retrying"
+done
+
+echo "Endpoint is up!"
---
+++ Health-check-RabbitMQ.sh
@@ -0,0 +1,6 @@
+﻿until curl --fail --silent --insecure --output /dev/null "https://localhost:15672/api/healthchecks/node"; do
+    sleep 5
+    echo "Retrying"
+done
+
+echo "Endpoint is up!"
