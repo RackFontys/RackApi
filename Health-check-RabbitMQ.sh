1f2630b8685e66cc046a9c1329daf0c1da36ac0a
JornKersten <jorn-2002@live.nl>
2024-05-22 13:32:01 +0200

    Changed checks

--- Health-check-RabbitMQ.sh
+++ Health-check-RabbitMQ.sh
@@ -1,4 +1,7 @@
-﻿until curl --fail --silent --insecure --output /dev/null "https://localhost:15672/api/healthchecks/node"; do
+﻿#!/bin/bash
+
+# Wait for the endpoint to return a 200 response
+until curl --fail --silent --insecure --output /dev/null "https://localhost:15672/api/healthchecks/node"; do
     sleep 5
     echo "Retrying"
 done
