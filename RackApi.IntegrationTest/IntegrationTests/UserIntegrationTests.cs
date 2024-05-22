4f751a82dfc3a220be061637b354ce7ee056ef0a
JornKersten <jorn-2002@live.nl>
2024-05-22 11:40:17 +0200

    Added health check to compose

--- RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
+++ RackApi.IntegrationTest/IntegrationTests/UserIntegrationTests.cs
@@ -6,7 +6,6 @@ namespace RackApi.IntegrationTest;
 public class UserIntegrationTests
 {
     private HttpClient _client;
-    private HttpClient _client2;
 
     [SetUp]
     public void Setup()
@@ -37,42 +36,10 @@ public class UserIntegrationTests
         // Assert
         response.EnsureSuccessStatusCode();
     }
-    
-    [SetUp]
-    public void SetupBasicUser()
-    {
-        // Initialize HttpClient
-        _client2 = new HttpClient();
-        _client2.BaseAddress = new Uri("http://localhost:5114");
-    }
-
-    [Test]
-    public async Task Test_User_Creation_User_Level()
-    {
-        // Arrange
-        var user = new 
-        { 
-            Id = 0,
-            Name = "Test User", 
-            Email = "test@example.com", 
-            CreatedAt = DateTime.UtcNow,
-            CompanyId = 1,
-            Password = "testpassword"
-        };
-        var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
-
-        // Act
-        var response = await _client2.PostAsync(_client2.BaseAddress, content);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-    }
 
     [TearDown]
     public void TearDown()
     {
-        // Initialize HttpClient
         _client.Dispose();
-        _client2.Dispose();
     }
 }
